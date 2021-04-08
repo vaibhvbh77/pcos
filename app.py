4ff4ddd7f8c536a31dcc854145c07c203635e93a
vaibhvbh77 <69358954+vaibhvbh77@users.noreply.github.com>
2021-04-08 17:39:50 +0530

    new

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 from flask import Flask,render_template,request
+# used request to catch the values of data in html
 # import pcos_model
+
+
 app=Flask(__name__)
 
 
 @app.route('/',methods=['GET','POST'])
 def basic():
-    # if request.method=='POST':
+    if request.method=='POST':
 
-    #     passenger_class=request.form['pclass']
-    #     age=request_template['age']
-    #     gender=request.form['gender']
-    #     if gender=="male":
-    #         male=1
-    #         female=0
-    #     else:
-    #         female=1
-    #         male=0
+        passenger_class=request.form['pclass']
+        age=request.form['age']
+        gender=request.form['gender']
+        if gender=="male":
+            male=1
+            female=0
+        else:
+            female=1
+            male=0
+        print(passanger_class,age,gender)    
     #     x_pred=[[passenger_class]]    
     #     imported_model=pcos_model.trained_model()
     #     y_pred=imported_model.predict(x_pred)
