fb54b811d9d0a8dec7700d48e392c6c0188ff3ec
vaibhvbh77 <69358954+vaibhvbh77@users.noreply.github.com>
2021-12-10 02:23:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,23 @@
 from flask import Flask, render_template, request
-import pcos_model.py
+from joblib import load
+import numpy as np
+
+model = load('model.joblib')
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 
-
 def basic():
 
     if request.method == 'POST':
+        input_data = [int(x) for x in request.form.values()]
+        x_pred = [np.array(input_data)]
 
+        prediction_value = model.predict(x_pred)
 
-        Diagnose = request.form['Diagnose']
-        Overweight = request.form['Overweight']
-        Weightgain = request.form['Weightgain]
-        Periods = request.form['Periods']
-        Conceiving = request.form['Conceiving']
-        AcneOrskinTag= request.form['AcneOrskinTag']
-        HairThinning= request.form['HairThinning']
-        DarkPatch = request.form['DarkPatch']
-        Tiredness = request.form['Tiredness']
-        MoodSwings = request.form['MoodSwings']
-        CannedFood = request.form['TCannedFood']
-        City = request.form['City']
-
-        y_pred = [[Diagnose, Overweight, Weightgain, Periods,Conceiving,AcneOrskinTag,HairThinning,DarkPatch,Tiredness,MoodSwings,CannedFood,TCannedFood]
-
-        trained_model = iris_model.training_model()
-
-        prediction_value = trained_model.predict(y_pred)
-
-        POC_not_detected = 'You have no Polycystic ovary syndrome'
-        POC_detected='You have no Polycystic ovary syndrome ,Please consult with an specialist ' 
-      
-
+        POC_not_detected = 'You are safe. You have no Polycystic ovary syndrome.'
+        POC_detected='You have Polycystic ovary syndrome. Please consult with a specialist.' 
 
         if prediction_value == 0:
             return render_template('index.html', POC_not_detected=POC_not_detected)
